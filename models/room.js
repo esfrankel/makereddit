c375ba9fa6d02e83636974f7f85d7a25eb9600da
Eric Frankel <eric-frankel@cox.net>
2018-07-06 14:43:29 -0700

    finished for real first room route

--- models/room.js
+++ models/room.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RoomSchema = new Schema({
-  topic: { topic: String, required: true}
+  topic: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Room', RoomSchema);
--- routes/rooms.js
+++ routes/rooms.js
@@ -11,7 +11,27 @@ router.get('/', (req, res, next) => {
 
 // Rooms new
 router.get('/new', auth.requireLogin, (req, res, next) => {
-
+  res.render('rooms/new');
 });
 
 // Rooms show
+router.get('/:id', auth.requireLogin, (req, res, next) => {
+
+});
+
+// Rooms edit
+router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
+
+});
+
+// Rooms update
+router.post('/:id', auth.requireLogin, (req, res, next) => {
+
+});
+
+// Rooms create
+router.post('/', auth.requireLogin, (req, res, next) => {
+
+});
+
+module.exports = router;
