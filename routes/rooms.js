ffcb17d94c4d89e2e8bb0d3b3ce20dcdb4d4f8fe
Eric Frankel <eric-frankel@cox.net>
2018-07-06 21:15:48 -0700

    finished room movements

--- routes/rooms.js
+++ routes/rooms.js
@@ -31,12 +31,20 @@ router.get('/:id', auth.requireLogin, (req, res, next) => {
 
 // Rooms edit
 router.get('/:id/edit', auth.requireLogin, (req, res, next) => {
+  Room.findById(req.params.id, function(err, room) {
+    if(err) { console.error(err) };
 
+    res.render('rooms/edit', { room: room });
+  });
 });
 
 // Rooms update
 router.post('/:id', auth.requireLogin, (req, res, next) => {
+  Room.findByIdAndUpdate(req.params.id, req.body, function(err, room) {
+    if(err) { console.error(err) };
 
+    res.redirect('/rooms/' + req.params.id);
+  });
 });
 
 // Rooms create
