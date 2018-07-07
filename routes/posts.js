f860090122a52a8a7496161de3e3aaeb5e9e9fb8
Eric Frankel <eric-frankel@cox.net>
2018-07-06 22:25:17 -0700

    added route for posts

--- routes/posts.js
+++ routes/posts.js
@@ -5,12 +5,29 @@ const Room = require('../models/room');
 
 // Posts new
 router.get('/new', auth.requireLogin, (req, res, next) => {
+  Room.findById(req.params.roomId, function(err, room) {
+    if(err) { console.error(err); }
 
+    res.render('posts/new', {room : room});
+  });
 });
 
 // Posts create
 router.post('/', auth.requireLogin, (req, res, next) => {
+  Room.findById(req.params.roomsId, function(err, room) {
+    if(err) { console.error(err); }
 
+    let post = new Post(req.body);
+    post.room = room;
+
+    post.save(function(err, post) {
+      if (err) {
+        console.error(err);
+      }
+
+      return res.redirect(`/rooms/${rooms._id}`);
+    });
+  });
 });
 
 module.exports = router;
