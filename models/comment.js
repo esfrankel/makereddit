cee1dbcaabc26f0a25d517aee9c8f83f010a509b
Eric Frankel <eric-frankel@cox.net>
2018-07-09 15:57:56 -0700

    L

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-  body: String
+  body: String,
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
