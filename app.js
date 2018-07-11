e7dd3097d376fddf26be552be362715a77158048
Eric Frankel <eric-frankel@cox.net>
2018-07-11 10:52:04 -0700

    added some linting improvements

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var rooms = require('./routes/rooms');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const rooms = require('./routes/rooms');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(session({ secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true }));
+app.use(session({
+  secret: 'secret-unique-code', cookie: { maxAge: 3600000 }, resave: true, saveUninitialized: true,
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,12 +30,12 @@ app.use('/users', usersRouter);
 app.use('/rooms', rooms);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,12 +45,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const mongoose = require('mongoose');
 const mongoURI = 'mongodb://ms-user:makeschool1@ds127781.mlab.com:27781/makereddit';
 
 mongoose.connect(mongoURI);
 mongoose.Promise = global.Promise;
-let db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = app;
